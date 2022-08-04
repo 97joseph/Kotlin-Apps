f96d0417b47f9d00ac5ceba856a84177606acc6f
Jeptoo080 <33089347+97joseph@users.noreply.github.com>
2022-08-04 16:44:39 +0300

    r

---
+++ datmusic-android-master/datmusic-android-master/buildSrc/src/main/java/tm/alashow/buildSrc/App.kt
@@ -0,0 +1,11 @@
+package tm.alashow.buildSrc
+
+object App {
+    const val id = "tm.alashow.datmusic"
+
+    const val compileSdkVersion = 31
+    const val targetSdkVersion = 31
+    const val minSdkVersion = 21
+    const val versionCode = 242
+    const val versionName = "2.2.4"
+}
